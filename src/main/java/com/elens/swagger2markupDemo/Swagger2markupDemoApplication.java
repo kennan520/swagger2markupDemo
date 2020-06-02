2e9db588403664a2c3602ba0cfbf37e5e39922c6
qiang <1308260332@qq.com>
2020-06-02 16:31:08 +0800

    swagger2markup

---
+++ src/main/java/com/elens/swagger2markupDemo/Swagger2markupDemoApplication.java
@@ -0,0 +1,13 @@
+package com.elens.swagger2markupDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2markupDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swagger2markupDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elens/swagger2markupDemo/Swagger2MarkupTest.java
@@ -0,0 +1,107 @@
+package com.elens.swagger2markupDemo;
+
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.io.BufferedWriter;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author ChaselX
+ * @date 2019/1/29 13:02
+ * <p>
+ * 完整异常：
+ * Unable to find a @SpringBootConfiguration, you need to use @ContextConfiguration or @SpringBootTest(classes=...) with your test
+ * 无法找到@SpringBootConfiguration，您需要在测试中使用@ContextConfiguration或@SpringBootTest(classes=…)
+ * <p>
+ * 问题产生的几种情况：
+ * 1.启动类 或者 程序入口类的包名和测试类所在包名不一致
+ * 例如：
+ * 启动类：com.leyou下的启动类
+ * 测试类：com.leyou.test下的测试类，“.test”多余
+ * <p>
+ * 2.程序入口类忘了添加：SpringBootApplication注解
+ * <p>
+ * 解决：
+ * 包名一致
+ * <p>
+ * 或者在@SpringBootTest(classes = com.xxx.class)中指定启动类
+ * <p>
+ * 日志配置，如果用logback.xml，然后application.yml中的指定，则会提示java.lang.IllegalStateException: Failed to load ApplicationContext
+ * 要改为logback-spring.xml才能默认加载
+ */
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@AutoConfigureRestDocs(outputDir = "build/asciidoc/snippets")
+@SpringBootTest
+@AutoConfigureMockMvc
+public class Swagger2MarkupTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //    @Test
+//    public void testApi() throws Exception {
+//        mockMvc.perform(get("/greeting")
+//                .accept(MediaType.APPLICATION_JSON))
+//                .andDo(document("greetingGet",
+//                        Preprocessors.preprocessResponse(Preprocessors.prettyPrint())))
+//                .andExpect(status().isOk());
+//    }
+    @org.junit.jupiter.api.Test
+    public void generateAsciiDocs() throws Exception {
+        //    输出Ascii格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withGeneratedExamples()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .build();
+
+        Swagger2MarkupConverter.from(new URL("http://localhost:8011/admin/api/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("src/docs/asciidoc/generated/index"));
+    }
+
+    @Test
+    public void createSpringfoxSwaggerJson() throws Exception {
+        System.out.println("hello");
+        String outputDir = System.getProperty("io.springfox.staticdocs.outputDir");
+        MvcResult mvcResult = this.mockMvc.perform(get("http://localhost:8011/admin/api/v2/api-docs")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String swaggerJson = response.getContentAsString();
+        Files.createDirectories(Paths.get(outputDir));
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputDir, "swagger.json"), StandardCharsets.UTF_8)) {
+            writer.write(swaggerJson);
+        }
+    }
+}
