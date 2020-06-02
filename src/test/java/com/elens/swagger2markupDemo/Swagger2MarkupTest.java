8176d479d144120293380a6c64eb6a4a37c4bf58
qiang <1308260332@qq.com>
2020-06-02 16:57:30 +0800

    添加jar下载地址

--- src/test/java/com/elens/swagger2markupDemo/Swagger2MarkupTest.java
+++ src/test/java/com/elens/swagger2markupDemo/Swagger2MarkupTest.java
@@ -4,33 +4,17 @@ import io.github.swagger2markup.Swagger2MarkupConfig;
 import io.github.swagger2markup.Swagger2MarkupConverter;
 import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
 import io.github.swagger2markup.markup.builder.MarkupLanguage;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.restdocs.operation.preprocess.Preprocessors;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import java.io.BufferedWriter;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Paths;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author ChaselX
@@ -86,22 +70,5 @@ public class Swagger2MarkupTest {
                 .withConfig(config)
                 .build()
                 .toFile(Paths.get("src/docs/asciidoc/generated/index"));
-    }
-
-    @Test
-    public void createSpringfoxSwaggerJson() throws Exception {
-        System.out.println("hello");
-        String outputDir = System.getProperty("io.springfox.staticdocs.outputDir");
-        MvcResult mvcResult = this.mockMvc.perform(get("http://localhost:8011/admin/api/v2/api-docs")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        MockHttpServletResponse response = mvcResult.getResponse();
-        String swaggerJson = response.getContentAsString();
-        Files.createDirectories(Paths.get(outputDir));
-        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputDir, "swagger.json"), StandardCharsets.UTF_8)) {
-            writer.write(swaggerJson);
-        }
     }
 }
